3077ce43e4d34ca9197cfc3fddd6f689ec3c1edd
Taylor Guo <taylorguo@126.com>
2020-07-27 12:22:44 +0800

    Update keras_pix2pix.py

--- GAN_Application/Image-to-Image_Translation/Keras/keras_pix2pix.py
+++ GAN_Application/Image-to-Image_Translation/Keras/keras_pix2pix.py
@@ -17,7 +17,45 @@ def define_discriminator(image_shape):
     merged = Concatenate()[src_image, target_image]
 
     # build D net
-    d = Conv2D(64, (4,4), strides=(2,2), padding="same", kernel_initializer=init)
+    # C64
+    d = Conv2D(64, (4, 4), strides=(2,2), padding="same", kernel_initializer=init)(merged)
     d = LeakyReLU(alpha=0.2)(d)
+    # C128
+    d = Conv2D(128, (4, 4), strides=(2, 2), padding="same", kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C256
+    d = Conv2D(256, (4, 4), strides=(2, 2), padding="same", kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C512
+    d = Conv2D(512, (4, 4), strides=(2, 2), padding="same", kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C512
+    d = Conv2D(512, (4, 4), strides=(2, 2), padding="same", kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # patch output
+    d = Conv2D(1, (4, 4), strides=(2, 2), padding="same", kernel_initializer=init)(d)
+    patch_out = Activation("sigmoid")(d)
+    # define Model
+    model = Model([src_image, target_image], patch_out)
+    model.summary()
+    # compile model
+    opt = keras.optimizers.Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss="binary_crossentropy", optimizer=opt, loss_weights=[0.5])
+    return model
 
+def encoder_block(layer_in, n_filers, batchnorm=True):
+    # weight initialization
+    init = keras.initializers.RandomNormal(stddev=0.02)
+    # add downsampleing layer
+    g = Conv2D(n_filers, (4, 4), strides=(2, 2), padding="same", kernel_initializer=init)(layer_in)
+    if batchnorm:
+        g = BatchNormalization()(g, training = True)
+    g = LeakyReLU(alpha=0.2)(g)
+    return g
 
+def decoder_block(layer_in, skip_in, n_filers, dropout=True):
+    init = keras.initializers.RandomNormal(stddev=0.02)
